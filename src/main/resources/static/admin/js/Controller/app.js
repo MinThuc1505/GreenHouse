5aec23b951be316f579a9bc19fc2688592dbb011
datmoi <93335350+datmoi@users.noreply.github.com>
2023-08-03 13:46:57 +0700

    [GreenHouse]: Admin Size

--- src/main/resources/static/admin/js/Controller/app.js
+++ src/main/resources/static/admin/js/Controller/app.js
@@ -6,3 +6,4 @@ app.constant('urlAccount', 'http://localhost:8081/rest/user');
 app.constant('urlProvider', 'http://localhost:8081/rest/provider');
 
 app.constant('urlProduct', 'http://localhost:8081/rest/products');
+app.constant('urlSize', 'http://localhost:8081/rest/sizes');
