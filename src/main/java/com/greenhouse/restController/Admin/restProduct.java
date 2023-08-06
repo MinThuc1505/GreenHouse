72bb22e80de351fc1f79adb48e3fead99c0a8c1d
datmoi <93335350+datmoi@users.noreply.github.com>
2023-08-07 00:45:38 +0700

    [GreenHouse] :100:

--- src/main/java/com/greenhouse/restController/Admin/restProduct.java
+++ src/main/java/com/greenhouse/restController/Admin/restProduct.java
@@ -1,11 +1,18 @@
 package com.greenhouse.restController.Admin;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,14 +20,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.greenhouse.DAO.ProductDAO;
 import com.greenhouse.dto.ProductDTO;
 import com.greenhouse.model.Product;
+
 @RestController
 @RequestMapping(value = "/rest/products")
 public class restProduct {
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @Autowired
     ProductDAO productDAO;
@@ -49,7 +67,11 @@ public class restProduct {
     }
 
     @PostMapping
-    private ResponseEntity<ProductDTO> create(@RequestBody ProductDTO productDTO) {
+    private ResponseEntity<ProductDTO> create(@RequestParam("productDTO") String productDTOJson,
+            @RequestParam("file") MultipartFile file) {
+        // Parse dữ liệu từ productDTOJson thành đối tượng ProductDTO
+        ProductDTO productDTO = new Gson().fromJson(productDTOJson, ProductDTO.class);
+
         // Kiểm tra các trường bắt buộc trước khi tạo sản phẩm
         if (productDTO.getId() != null && productDAO.existsById(productDTO.getId())) {
             return ResponseEntity.badRequest().build();
@@ -58,6 +80,19 @@ public class restProduct {
         // Chuyển đổi từ DTO sang Entity để lưu vào cơ sở dữ liệu
         Product product = new Product();
         BeanUtils.copyProperties(productDTO, product);
+
+        // Lưu tệp ảnh vào thư mục /src/main/resources/static
+        try {
+            String fileName = file.getOriginalFilename();
+            String filePath = Paths.get("").toAbsolutePath().toString() + "/src/main/resources/static/images/" + fileName;
+            File dest = new File(filePath);
+            file.transferTo(dest);
+            product.setImage(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+
         Product savedProduct = productDAO.save(product);
 
         // Chuyển đổi từ Entity sang DTO để trả về kết quả
@@ -68,7 +103,11 @@ public class restProduct {
     }
 
     @PutMapping(value = "/{id}")
-    private ResponseEntity<ProductDTO> update(@PathVariable("id") Integer id, @RequestBody ProductDTO productDTO) {
+    private ResponseEntity<ProductDTO> update(@PathVariable("id") Integer id,
+            @RequestParam("productDTO") String productDTOJson,
+            @RequestParam(value = "file", required = false) MultipartFile file) {
+        // Parse dữ liệu từ productDTOJson thành đối tượng ProductDTO
+        ProductDTO productDTO = new Gson().fromJson(productDTOJson, ProductDTO.class);
         if (!productDAO.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
@@ -81,6 +120,19 @@ public class restProduct {
         // Chuyển đổi từ DTO sang Entity để cập nhật vào cơ sở dữ liệu
         Product product = new Product();
         BeanUtils.copyProperties(productDTO, product);
+
+        // Lưu tệp ảnh vào thư mục /src/main/resources/static
+        try {
+            String fileName = file.getOriginalFilename();
+            String filePath = Paths.get("").toAbsolutePath().toString() + "/src/main/resources/static/images/" + fileName;
+            File dest = new File(filePath);
+            file.transferTo(dest);
+            product.setImage(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+
         product.setId(id); // Đảm bảo cập nhật cho sản phẩm với id đã cho
         Product updatedProduct = productDAO.save(product);
 
--- src/main/resources/static/admin/js/Controller/productCtrl.js
+++ src/main/resources/static/admin/js/Controller/productCtrl.js
@@ -4,8 +4,8 @@ app.controller('productCtrl', function($scope, $http, urlProduct) {
     $scope.items = {};
     $scope.sizes = [];
     $scope.materials = [];
+    $scope.selectedItemIndex = -1; // Biến lưu trạng thái sản phẩm đang được chỉnh sửa
 
-    
     $scope.load_all = function() {
         var url = `${host}`;
         $http.get(url).then(resp => {
@@ -14,8 +14,9 @@ app.controller('productCtrl', function($scope, $http, urlProduct) {
             console.log("Error", Error);
         });
     };
-       // Lấy dữ liệu kích thước
-       $http.get('/rest/sizes').then(resp => {
+
+    // Lấy dữ liệu kích thước
+    $http.get('/rest/sizes').then(resp => {
         $scope.sizes = resp.data;
     }).catch(Error => {
         console.log("Error", Error);
@@ -27,19 +28,71 @@ app.controller('productCtrl', function($scope, $http, urlProduct) {
     }).catch(Error => {
         console.log("Error", Error);
     });
+
+   $scope.Edit = function(key, index) {
+    var url = `${host}/${key}`;
+    $http.get(url).then(resp => {
+        $scope.form = resp.data;
+        $scope.selectedItemIndex = index; // Lưu chỉ số sản phẩm đang được chỉnh sửa
+        displayImages(resp.data.image); // Hiển thị ảnh tương ứng cho sản phẩm đang chỉnh sửa
+    }).catch(Error => {
+        console.log("Error", Error);
+    });
+};
     
-    $scope.Edit = function(key) {
-        var url = `${host}/${key}`;
-        $http.get(url).then(resp => {
-            $scope.form = resp.data;
-            $scope.key = key;
-              }).catch(Error => {
-            console.log("Error", Error);
+    
+    $scope.Create = function() {
+        var formData = new FormData();
+        formData.append("name", $scope.name);
+        formData.append("price", $scope.price);
+        formData.append("quantity", $scope.quantity);
+        formData.append("status", $scope.status);
+        formData.append("size", $scope.size);
+        formData.append("material", $scope.material);
+        formData.append("description", $scope.description);
+        formData.append("file", document.getElementById('fileInputCreateProduct').files[0]);
+    
+        formData.append("productDTO", JSON.stringify({
+            name: $scope.name,
+            price: $scope.price,
+            quantity: $scope.quantity,
+            status: $scope.status,
+            size: $scope.size,
+            material: $scope.material,
+            description: $scope.description
+        }));
+        formData.append("file", $scope.form.image);
+        var url = `${host}`;
+        $http.post(url, formData, {
+            headers: { 'Content-Type': undefined }
+        }).then(resp => {
+            console.log("Success", resp);
+            $scope.load_all();
+            Swal.fire({
+                icon: 'success',
+                title: 'Thành công',
+                text: `Đã thêm sản phẩm ` + $scope.name,
+            });
+        }).catch(Error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Thất bại',
+                text: `Thêm sản phẩm ` + $scope.name + ` thất bại `,
+            });
         });
     };
-    
     $scope.Update = function(key) {
-        var item = {
+        var formData = new FormData();
+        formData.append("name", $scope.form.name);
+        formData.append("price", $scope.form.price);
+        formData.append("quantity", $scope.form.quantity);
+        formData.append("status", $scope.form.status);
+        formData.append("size", $scope.form.size);
+        formData.append("material", $scope.form.material);
+        formData.append("description", $scope.form.description);
+        formData.append("file", document.getElementById('fileInput22').files[0]);
+    
+        formData.append("productDTO", JSON.stringify({
             id: $scope.form.id,
             name: $scope.form.name,
             price: $scope.form.price,
@@ -47,12 +100,14 @@ app.controller('productCtrl', function($scope, $http, urlProduct) {
             status: $scope.form.status,
             size: $scope.form.size,
             material: $scope.form.material,
-            image: $scope.form.image, 
             description: $scope.form.description
-        };
-
+        }));
+        formData.append("file", $scope.form.image);
+    
         var url = `${host}/${key}`;
-        $http.put(url, item).then(resp => {
+        $http.put(url, formData, {
+            headers: { 'Content-Type': undefined }
+        }).then(resp => {
             $scope.items[$scope.key] = resp.data;
             $scope.load_all();
             Swal.fire({
@@ -69,39 +124,6 @@ app.controller('productCtrl', function($scope, $http, urlProduct) {
         });
     };
     
-    $scope.Create = function() {
-        var files = document.getElementById('fileInputCreateProduct').files;
-        console.log(files[0].name);
-        var item = {
-            name: $scope.name,
-            price: $scope.price,
-            quantity: $scope.quantity,
-            status: $scope.status,
-            size: $scope.size,
-            material: $scope.material,
-            image: files[0].name, 
-             description: $scope.description
-        };
-
-        console.log(item);
-
-        var url = `${host}`;
-        $http.post(url, item).then(resp => {
-            console.log("Success", resp);
-            $scope.load_all();
-            Swal.fire({
-                icon: 'success',
-                title: 'Thành công',
-                text: `Đã thêm sản phẩm ` + item.name,
-            });
-        }).catch(Error => {
-            Swal.fire({
-                icon: 'error',
-                title: 'Thất bại',
-                text: `Thêm sản phẩm ` + item.name + ` thất bại `,
-            });
-        });
-    };
     
     $scope.Delete = function(key) {
         var url = `${host}/${key}`;
@@ -124,8 +146,32 @@ app.controller('productCtrl', function($scope, $http, urlProduct) {
     $scope.load_all();
 });
 
-function displayImageProduct(event, idImg) {
-    var image = document.getElementById(idImg);
-    image.src = URL.createObjectURL(event.target.files[0]);
-    image.style.display = "block";
+function displayImage(event) {
+    var input = event.target;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            var imageContainer = document.getElementById('uploadedImage');
+            imageContainer.src = e.target.result;
+            imageContainer.style.display = "block";
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function displayImages(event) {
+    var input = event.target;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            var imageContainer = document.getElementById('uploadedImages');
+            imageContainer.src = e.target.result;
+            imageContainer.style.display = "block";
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
 }
