5887e350a18b1828e82edb04f18a0f57698bd519
datmoi <93335350+datmoi@users.noreply.github.com>
2023-08-09 21:24:28 +0700

    [GreenHouse] Update PriceHistory

--- src/main/java/com/greenhouse/DAO/PriceHistoryDAO.java
+++ src/main/java/com/greenhouse/DAO/PriceHistoryDAO.java
@@ -2,7 +2,11 @@ package com.greenhouse.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.greenhouse.model.PriceHistory;
+import com.greenhouse.model.Product;
+
+import jakarta.transaction.Transactional;
 
 public interface PriceHistoryDAO extends JpaRepository<PriceHistory, Integer> {
-
+    @Transactional
+  void deleteByProduct(Product product);
 }
