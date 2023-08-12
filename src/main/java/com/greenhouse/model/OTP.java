358e970d65d6279304c4ab51b921f2ba65dbad65
MinThuc1505 <minhthuc7a1@gmail.com>
2023-08-12 21:10:08 +0700

    [Green House] - Update send OTP in Forgot Password

--- src/main/java/com/greenhouse/model/OTP.java
+++ src/main/java/com/greenhouse/model/OTP.java
@@ -27,17 +27,18 @@ public class OTP implements Serializable{
     @Column(name = "OTPCode")
     private String otpCode;
 
-    @Column(name = "ExpirationTime")
+    @Column(name = "Expiration_Time")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date expirationTime;
 
     @Column(name = "Status")
-    private String status;
+    private int status;
 
-    @Column(name = "CreateAt")
+    @Column(name = "Create_At")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createAt;
 
-    @Column(name = "UpdateAt")
+    @Column(name = "Update_At")
     @Temporal(TemporalType.TIMESTAMP)
     private Date updateAt;
 }
