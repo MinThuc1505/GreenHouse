771668d99d7a26aec2d807ec3185d1c1680fa90e
ZonNull <110268408+ZonNull@users.noreply.github.com>
2023-08-14 03:51:51 +0700

    Admin (change html index ,fix print bill)

--- src/main/resources/static/admin/js/Controller/indexController.js
+++ src/main/resources/static/admin/js/Controller/indexController.js
@@ -88,15 +88,15 @@ app.controller('indexController', function ($scope, $http, indexController) {
         });
     };
 
-    $scope.load_Product = function () {
-        var url = `${host}/newProduct`;
-        $http.get(url).then(resp => {
-            $scope.items = resp.data;
-            console.log("Success", $scope.items);
-        }).catch(error => {
-            console.log("Error", error);
-        });
-    };
+    // $scope.load_Product = function () {
+    //     var url = `${host}/newProduct`;
+    //     $http.get(url).then(resp => {
+    //         $scope.items = resp.data;
+    //         console.log("Success", $scope.items);
+    //     }).catch(error => {
+    //         console.log("Error", error);
+    //     });
+    // };
 
     $scope.getRevenueData2 = function () {
         var url = `${host}/year`;
@@ -157,7 +157,7 @@ app.controller('indexController', function ($scope, $http, indexController) {
                 roles.push(role);
                 counts.push(data[i][1]);
             }
-            
+
             // Tạo biểu đồ tròn
             var ctx = document.getElementById('barRoleChart').getContext('2d');
             var chart = new Chart(ctx, {
@@ -181,21 +181,71 @@ app.controller('indexController', function ($scope, $http, indexController) {
                             position: 'bottom',
                             fontSize: 16
                         }
-                    }
+                    },
+                    maintainAspectRatio: true, // Tắt tỷ lệ giữa chiều rộng và chiều cao
+                    aspectRatio: 1.6// Tỷ lệ giữa chiều cao và chiều rộng (ở đây ví dụ là 3:2)
                 }
             });
         }).catch(function (error) {
             console.log(error);
         });
     };
-    
+
 
     $scope.getRevenue();
     $scope.getTotalDiscounts();
     $scope.getTotalUsers();
     $scope.getTotalProducts();
     $scope.loadAllData();
-    $scope.load_Product();
+    // $scope.load_Product();
     $scope.getRevenueData1();
     $scope.getRevenueData2();
 });
+const today = moment();
+let currentMonth = today.clone();
+
+function renderMonthlyCalendar() {
+    const monthlyCalendarBody = document.getElementById('monthly-calendar-body');
+    monthlyCalendarBody.innerHTML = '';
+
+    const firstDayOfMonth = currentMonth.clone().startOf('month').startOf('isoWeek');
+
+    for (let week = 0; week < 6; week++) {
+        const weekRow = document.createElement('tr');
+        for (let dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++) {
+            const day = firstDayOfMonth.clone().add(week * 7 + dayOfWeek, 'days');
+            const dayCell = document.createElement('td');
+            dayCell.textContent = day.format('D');
+            dayCell.classList.add('day-cell');
+            if (day.isSame(today, 'day')) {
+                dayCell.classList.add('current-day');
+            }
+            if (day.month() !== currentMonth.month()) {
+                dayCell.classList.add('other-month-day');
+            } else if (!day.isSame(today, 'day')) {
+                dayCell.classList.add('current-month-day');
+            }
+            weekRow.appendChild(dayCell);
+        }
+        monthlyCalendarBody.appendChild(weekRow);
+    }
+}
+
+function updateCurrentMonthText() {
+    document.getElementById('current-month').textContent = currentMonth.format('dddd, D MMMM YYYY');
+}
+
+document.getElementById('prev-month').addEventListener('click', () => {
+    currentMonth.subtract(1, 'month');
+    renderMonthlyCalendar();
+    updateCurrentMonthText();
+});
+
+document.getElementById('next-month').addEventListener('click', () => {
+    currentMonth.add(1, 'month');
+    renderMonthlyCalendar();
+    updateCurrentMonthText();
+});
+
+renderMonthlyCalendar();
+updateCurrentMonthText();
